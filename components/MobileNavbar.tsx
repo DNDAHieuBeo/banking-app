018ba5d924d629ec08b709621a9519b24919201e
Hieu Le <mac@Hieu-Le.local>
2024-09-12 21:27:57 +0700

    feat: create sidebar and mobilenav

---
+++ components/MobileNavbar.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+import { cn } from '../lib/utils';
+
+const MobileNavbar = ({ user }: MobileNavProps) => {
+  const pathname = usePathname();
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            width={30}
+            height={30}
+            alt="abc"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-white">
+          <Link
+            href="/"
+            className=" flex cursor-pointer items-center gap-1 px-4"
+          >
+            <Image src="/icons/logo.svg" width={34} height={34} alt="ngu" />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+              Horizon
+            </h1>
+          </Link>
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="h-full flex flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathname === item.route ||
+                    pathname.startsWith(`${item.route}/`);
+                  return (
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={`mobilenav-sheet_close w-full ${
+                          isActive ? "bg-bank-gradient" : ""
+                        }`}
+                      >
+                      
+                          <Image
+                            src={item.imgURL}
+                            alt={item.label}
+                            width={20}
+                            height={20}
+                            className={
+                              isActive ? "brightness-[3] invert-0" : ""
+                            }
+                          />
+                    
+                        <p
+                         className={`${isActive ? "text-white" : "text-black-2"} text-[16] font-semibold`}
+
+
+                        >
+                          {item.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNavbar;
